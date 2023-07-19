3f06ce6f1592e0cd78837f320553b7e17e68d5f9
gabrielpadma <yggderasil@gmail.com>
2023-07-19 13:24:02 +0700

    final Palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,13 @@
 function palindrome(str) {
-  let newStr = str.match(/[a-zA-Z0-9]+/g);
-  console.log(newStr);
+  let newStr = str.match(/[a-zA-Z0-9]+/g).join("");
+  for (let i = 0; i < newStr.length / 2; i++) {
+    let curChar = i;
+    let charAkhir = newStr.length - i - 1;
+    if (newStr[curChar].toLowerCase() !== newStr[charAkhir].toLowerCase())
+      return false;
+  }
+
   return true;
 }
 
-palindrome("0_0 (: /- :) 0-0");
+console.log(palindrome("_eye"));
